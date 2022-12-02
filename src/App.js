20fa356b30605f3ce5d603a4564043fb8cac656e
Gokul Krishna <gokulkrishnatu11@gmail.com>
2022-12-02 19:46:01 +0600

    intergrated youtube

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Navbar from './components/Navbar'
 import "./App.css"
 import Banner from './components/Banner'
 import RowPost from './components/RowPost'
+import {Actions,orginals} from "./urls"
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Banner/>
-      <RowPost/> 
+      <RowPost url={orginals}title='Netflix Orginals'/> 
+      <RowPost url={Actions}title='Actions' isSmall/> 
     </div>
   )
 }
